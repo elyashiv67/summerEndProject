81d6ebebb53fa4befae45a8112df74001e3dc624
elyashiv67 <elishivs5@gmail.com>
2025-09-15 20:13:10 +0300

    opened a local server and checked its working

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3894;
+
+app.use(express.static(__dirname))
+app.use(express.json());
+
+app.get('/',(req,res)=>{res.sendFile(__dirname+'/public/index.html')})
+
+
+app.listen(port,()=>{console.log(`http://localhost:${port}`)})
